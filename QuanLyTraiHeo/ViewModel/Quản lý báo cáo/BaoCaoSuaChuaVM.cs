6825fcf2d3722eb3ddcaa59b36349d05d9b7e75e
NBin17 <83058853+dkhoa1709@users.noreply.github.com>
2022-11-20 14:39:57 +0700

    finish future export data to file Excel

--- QuanLyTraiHeo/ViewModel/Quản lý báo cáo/BaoCaoSuaChuaVM.cs
+++ QuanLyTraiHeo/ViewModel/Quản lý báo cáo/BaoCaoSuaChuaVM.cs
@@ -1,28 +1,57 @@
 ﻿using LiveCharts;
 using LiveCharts.Wpf;
+using Microsoft.Win32;
+using OfficeOpenXml;
+using QuanLyTraiHeo.Model;
+using QuanLyTraiHeo.View.Windows;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using static QuanLyTraiHeo.ViewModel.BaoCaoTinhTrangHeoVM;
+using LicenseContext = OfficeOpenXml.LicenseContext;
+using SeriesCollection = LiveCharts.SeriesCollection;
+using System.Windows.Controls;
+using DataTable = System.Data.DataTable;
+using System.Windows.Data;
+using OfficeOpenXml.Style;
 
 namespace QuanLyTraiHeo.ViewModel
 {
-    public class BaoCaoSuaChuaVM:BaseViewModel
+    public class BaoCaoSuaChuaVM : BaseViewModel
     {
+        #region Attributes
         private Func<ChartPoint, string> pointLabel;
+        public List<PHIEUSUACHUA> lstPhieu = new List<PHIEUSUACHUA>();
+        private DataTable table = new DataTable(DataProvider.Ins.DB.PHIEUSUACHUAs.Sql);
+        public DataGrid dataGrid = new DataGrid();
+        #endregion
+
+        #region Property
         public Func<ChartPoint, string> PointLabel { get => pointLabel; set { pointLabel = value; OnPropertyChanged(); } }
+        public List<PHIEUSUACHUA> LstPhieu { get => lstPhieu; set { lstPhieu = value; OnPropertyChanged(); } }
         public SeriesCollection SeriesCollectionTSheoChart { get; set; }
         public string[] LabelsTSheoChart { get; set; }
-        public List<Phieu> LstPhieu = new List<Phieu>();
-        public List<Phieu> lstPhieu { get=>LstPhieu; set { LstPhieu = value; OnPropertyChanged(); } }
+        #endregion
+
+        #region Command
+        public ICommand XuatFileExcelCommand { get; set; }
+        #endregion
+
         public BaoCaoSuaChuaVM()
         {
+            LstPhieu = DataProvider.Ins.DB.PHIEUSUACHUAs.ToList();
+            DataSet temp = new DataSet();
+            dataGrid.ItemsSource = table.DefaultView;
             PointLabel = chartPoint =>
                 string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
-
             #region binding dữ liệu cho Biểu đồ tổng số chuồng trại đã sửa chửa
             SeriesCollectionTSheoChart = new SeriesCollection
             {
@@ -35,21 +64,83 @@ namespace QuanLyTraiHeo.ViewModel
 
             LabelsTSheoChart = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
             #endregion
-            #region binding dữ liệu cho danh sách phiếu
-            lstPhieu.Add(new Phieu() { STT = "1", IDPhieu = "SP01", IDChuong = "CH01", Ngay = "13/11/2022", NhanVien = "Trần Trung Thành", TongTien = "100,000 VND", Ghichu = "Hư 1 đèn sưởi" });
-            lstPhieu.Add(new Phieu() { STT = "", IDPhieu = "", IDChuong = "", Ngay = "", NhanVien = "TỔNG TIỀN", TongTien = "100,000 VND", Ghichu = "" });
-            #endregion
-        }
-        public class Phieu
-        {
-            public string STT { get; set; }
-            public string IDPhieu { get; set; }
-            public string IDChuong { get; set; }
-            public string Ngay { get; set; }
-            public string NhanVien { get; set; }
-            public string TongTien { get; set; }
-            public string Ghichu { get; set; }
+            XuatFileExcelCommand = new RelayCommand<ListView>((p) => { return true; }, (p) =>
+            {
+                string filePath = "";
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "Excel | *.xlsx";
+                if(dialog.ShowDialog() == true)
+                {
+                    filePath = dialog.FileName;
+                }
+                if(string.IsNullOrEmpty(filePath))
+                {
+                    MessageBox.Show("Đường dẫn không hợp lệ!");
+                    return;
+                }
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                try
+                {
+                    using (ExcelPackage excelPackage = new ExcelPackage())
+                    {
+                        excelPackage.Workbook.Properties.Author = "Dang Khoa";
+                        excelPackage.Workbook.Properties.Title = "Báo cáo sửa chữa";
+                        excelPackage.Workbook.Worksheets.Add("Báo cáo");
+                        ExcelWorksheet ws = excelPackage.Workbook.Worksheets[0];
+                        ws.Name = "Báo cáo";
+                        ws.Cells.Style.Font.Size = 15;
+                        ws.Cells.Style.Font.Name = "Times New Roman";
+                        string[] arrColumnHeader = {
+                            "Số phiếu",
+                            "Ngày lập phiếu",
+                            "Mã nhân viên",
+                            "Mã đối tác",
+                            "Ghi chú",
+                            "Trạng thái",
+                            "Tổng tiền"
+                        };
+                        var countColHeader = arrColumnHeader.Count();
+                        ws.Cells[1, 1].Value = "Báo cáo sửa chữa";
+                        ws.Cells[1, 1, 1, countColHeader].Merge = true;
+                        ws.Cells[1, 1, 1, countColHeader].Style.Font.Bold = true;
+                        ws.Cells[1, 1, 1, countColHeader].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        int colIndex = 1;
+                        int rowIndex = 2; 
+                        foreach(var item in arrColumnHeader)
+                        {
+                            var cell = ws.Cells[rowIndex, colIndex];
+                            var fill = cell.Style.Fill;
+                            var x = cell.Style.WrapText;
+                            fill.PatternType = ExcelFillStyle.Solid;
+                            fill.BackgroundColor.SetColor(System.Drawing.Color.LightBlue);
+                            var border = cell.Style.Border;
+                            border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+                            cell.Value = item;
+                            colIndex++;
+                        }
+                        foreach(var item in LstPhieu)
+                        {
+                            colIndex = 1;
+                            rowIndex++;
+                            ws.Cells[rowIndex, colIndex++].Value = item.SoPhieu.ToString();
+                            ws.Cells[rowIndex, colIndex++].Value = item.NgaySuaChua.ToString();
+                            ws.Cells[rowIndex, colIndex++].Value = item.MaNhanVien.ToString();
+                            ws.Cells[rowIndex, colIndex++].Value = item.MaDoiTac.ToString();
+                            ws.Cells[rowIndex, colIndex++].Value = item.GhiChu.ToString();
+                            ws.Cells[rowIndex, colIndex++].Value = item.TrangThai.ToString();
+                            ws.Cells[rowIndex, colIndex++].Value = item.TongTien.ToString();
+                        }
+                        Byte[] bin = excelPackage.GetAsByteArray();
+                        File.WriteAllBytes(filePath, bin);
+                    }
+                    MessageBox.Show("Xuất file excel thành công!");
+                }
+                catch
+                {
+                    MessageBox.Show("Có lỗi xảy ra khi xuất file excel!");
+                }
+            });
         }
     }
-    
+
 }
